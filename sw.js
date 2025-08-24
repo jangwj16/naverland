679ad64d8bd8c6f6c81361b47f3b6e6c0df2b992
jangwj16 <jangwj16@gmail.com>
2025-08-24 14:52:19 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('landlink-v1').then(c=>c.addAll([
+    './','./index.html','./share.html','./manifest.webmanifest',
+    './icons/icon-192.png','./icons/icon-512.png'
+  ])));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
