8b5a4e71a2290d9b7bd8e4015bc3110cc8a89c13
jangwj16 <jangwj16@gmail.com>
2025-08-25 14:03:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,5 @@ self.addEventListener('install', e=>{
   ])));
 });
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
